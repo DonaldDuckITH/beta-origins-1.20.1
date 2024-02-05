63d4b74e5476f31d392a061b8dfe89e80a65132c
DonaldDuckITH <donaldduckith@gmail.com>
2024-02-05 11:21:53 +1100

    Initial commit

---
+++ src/main/java/net/donaldduckith/betaorigins/BetaOriginsClient.java
@@ -0,0 +1,10 @@
+package net.donaldduckith.betaorigins;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetaOriginsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
