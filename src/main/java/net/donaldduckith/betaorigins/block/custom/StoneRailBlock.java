33e33eb470b9bb3e87c2576d2c594f1f711e273f
DonaldDuckITH <donaldduckith@gmail.com>
2024-02-05 13:56:54 +1100

    Fixed stone rail textures. Added class StoneRailBlock

---
+++ src/main/java/net/donaldduckith/betaorigins/block/custom/StoneRailBlock.java
@@ -0,0 +1,10 @@
+package net.donaldduckith.betaorigins.block.custom;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.RailBlock;
+
+public class StoneRailBlock extends RailBlock {
+    public StoneRailBlock(AbstractBlock.Settings settings) {
+        super(settings);
+    }
+}
